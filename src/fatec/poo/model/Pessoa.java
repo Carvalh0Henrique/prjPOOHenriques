277f8fd79d0232fcce3261b0b109b3bb0599dac6
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-11-07 10:17:50 -0300

    feat(model): Implementa a classe Pessoa, closes #2

--- src/fatec/poo/model/Pessoa.java
+++ src/fatec/poo/model/Pessoa.java
@@ -6,5 +6,39 @@ package fatec.poo.model;
  * @author Henrique Carvalho
  */
 public class Pessoa {
+    private String cpf;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
     
+    //SETTERS
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }    
+    
+    //GETTERS
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }   
 }
