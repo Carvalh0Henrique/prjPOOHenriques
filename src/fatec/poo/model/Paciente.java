022394da3a73a3decc7a6ed628238d9631da5f43
Henrique A. R. <henrique4r.it@gmail.com>
2025-11-10 23:12:37 -0300

    feat(Paciente): Implementa a classe Paciente, closes #4

---
+++ src/fatec/poo/model/Paciente.java
@@ -0,0 +1,53 @@
+package fatec.poo.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Henrique Andrade
+ */
+public class Paciente extends Pessoa {
+    
+    private LocalDate dataNascimento ;
+    private double altura;
+    private double peso;
+
+    private Paciente(String cpf, String nome, LocalDate dataNascimento){
+        super(cpf, nome);
+        this.dataNascimento = dataNascimento;
+    }
+
+
+    //SETTERS
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+    //GETTERS
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double calcIMC(){
+        return getPeso() / Math.pow(getAltura(),2);
+    }
+
+    public int calcIdade(LocalDate dataAtual){
+        return  dataAtual.compareTo(getDataNascimento());
+    }
+    
+    
+}
