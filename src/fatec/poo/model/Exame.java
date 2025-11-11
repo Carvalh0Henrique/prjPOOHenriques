7b71c7926903ee55e2a732b8d8428952f1b97e8c
Henrique A. R. <henrique4r.it@gmail.com>
2025-11-11 00:00:50 -0300

    feat(Exame): Implementa a classe Exame, closes #6

---
+++ src/fatec/poo/model/Exame.java
@@ -0,0 +1,54 @@
+package fatec.poo.model;
+
+public class Exame {
+    
+    private int codigo;
+    private String descricao;
+    private String data;
+    private String horario;
+    private double valor;
+
+
+    public Exame(int codigo, String descricao){
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+    
+    
+    //SETTERS
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+    //GETTERS
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    
+}
