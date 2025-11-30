14043a566c58dac31fed1feae50976c982b38d21
Henrique A. R. <henrique4r.it@gmail.com>
2025-11-30 08:51:38 -0300

    feat(GuiPreescreverMedicacao): adiciona o Data Access Object de Medicacao"

---
+++ src/fatec/poo/control/DaoMedicacao.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.control;
+
+import fatec.poo.model.Medicacao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author h-and-rod
+ */
+public class DaoMedicacao {
+    
+    private Connection conn;
+    
+    public DaoMedicacao(Connection conn) {
+        this.conn = conn;
+    }
+    
+    public void inserir (Medicacao medicacao){
+        PreparedStatement ps = null;
+        
+        try{
+            ps = conn.prepareStatement("INSERT INTO tbMedicacao(nome, dosagem, qtdeDias) VALUES (?, ?, ?)");
+            
+            ps.setString(1, medicacao.getNome());
+            ps.setString(2, medicacao.getDosagem());
+            ps.setInt(3, medicacao.getQtdeDias());
+            
+            ps.execute();
+        }catch (SQLException ex){
+            System.out.println(ex.toString());
+        }
+    }
+    
+    public Medicacao consultar(String nome){
+        
+       Medicacao medicacao = null;
+       PreparedStatement ps = null;
+       
+       try{
+           ps = conn.prepareStatement("SELECT * FROM tbMedicacao WHERE " + "nome = ?");
+           ps.setString(1, nome);
+           
+           ResultSet rs = ps.executeQuery();
+           
+           if (rs.next()){
+               medicacao = new Medicacao(nome);
+               medicacao.setDosagem(rs.getString("dosagem"));
+               medicacao.setQtdeDias(rs.getInt("qtdeDias"));
+           }                             
+       } catch (SQLException ex){
+           System.out.println(ex.toString());
+       }
+       
+       return medicacao;
+              
+    }
+    
+    public void alterar(Medicacao medicacao){
+        PreparedStatement ps = null;
+        
+        try{
+            ps = conn.prepareStatement("UPDATE tbMedicacao SET dosagem = ?, qtdeDias = ? " + "WHERE nome = ?");
+            
+            ps.setString(1, medicacao.getDosagem());
+            ps.setInt(2, medicacao.getQtdeDias());
+            ps.setString(3, medicacao.getNome());
+            
+            ps.execute();
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+            
+        }
+    }
+    
+    public void excluir(Medicacao medicacao){
+        PreparedStatement ps = null;
+        
+        try{
+           ps = conn.prepareStatement("DELETE FROM tbMedicacao WHERE nome = ?");
+           
+           ps.setString(1, medicacao.getNome());
+           
+           ps.execute();
+           
+        } catch (SQLException ex){
+            System.out.println(ex.toString());
+        }
+    }
+    
+    
+}
